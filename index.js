148ee547ab3f1c3b0902cd5ddb6c431c6d09828f
Forellko <dragonforce97q@gmail.com>
2022-12-07 14:25:22 +0300

    create h1 element

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+// function MainContent() {
+//   return <h1>I'm learning React!</h1>;
+// }
+
+// ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+
+const h1 = document.createElement('h1');
+h1.textContent = 'This is an imperative way to program';
+h1.className = 'header';
+document.getElementById('root').append(h1);
