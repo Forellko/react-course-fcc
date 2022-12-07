f3880345e73c3fe30c7b85b7cb2d5246d60d1858
Forellko <dragonforce97q@gmail.com>
2022-12-07 14:29:21 +0300

    delete h1 element

--- index.js
+++ index.js
@@ -2,9 +2,5 @@
 //   return <h1>I'm learning React!</h1>;
 // }
 
-// ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
-
-const h1 = document.createElement('h1');
-h1.textContent = 'This is an imperative way to program';
-h1.className = 'header';
-document.getElementById('root').append(h1);
+ReactDOM.render(<h1>This is JSX</h1>, document.getElementById('root'));
+  
