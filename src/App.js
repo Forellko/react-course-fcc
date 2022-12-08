1e8340e5d2233a332b547a020600ce5f4ae7f795
Forellko <dragonforce97q@gmail.com>
2022-12-08 06:43:48 +0300

    Add Header Content Footer components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
